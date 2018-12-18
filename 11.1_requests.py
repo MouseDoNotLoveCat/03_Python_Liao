77aaf624b1ae0c7bfb59881ef88a8e43c96d1690
[MouseDoNotLoveCat] <[luhow@126.com]>
2018-12-18 21:16:32 +0800

    03

---
+++ 11.1_requests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @File  : 11.1_requests.py
+# @Author: Luhow
+# @Date  : 2018/12/18
+# @Desc  : requests
+
+
+import requests
+r = requests.get('https://www.douban.com/')
+# print(r.status_code)
+# print(r.text)
